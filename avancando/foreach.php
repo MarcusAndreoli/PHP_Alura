cf4ad542c523b8ec07eb75d6bbc8291e53006fde
Marcus Andreoli <63010655+MarcusAndreoli@users.noreply.github.com>
2023-04-12 21:51:21 -0300

    Atualizando o arquivo foreach.php

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -2,23 +2,27 @@
 
 $contasCorrentes = [
     //$cpf => $conta
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => "Marcus",
         'saldo' => 1000
     ],
-    14235678911 => [
+    '142.356.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    76543218941 => [
+    '765.432.189-41' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ]
 ];
 
+$contasCorrentes['654.321.789-13'] = [
+    'titular' => 'José',
+    'saldo' => 3000
+];
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    //echo $cpf . PHP_EOL;
-    echo $conta['titular'] . PHP_EOL;
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
 
 ?>
