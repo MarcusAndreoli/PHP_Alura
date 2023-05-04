9ab60f23db02c33d80c0cef3d688995bbb69f43f
Marcus Andreoli <63010655+MarcusAndreoli@users.noreply.github.com>
2023-05-04 11:32:06 -0300

    Upando arquivos da aula 4

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     //$cpf => $conta
     '123.456.789-10' => [
@@ -16,34 +18,12 @@ $contasCorrentes = [
     ]
 ];
 
-function sacar(array $conta, float $valorASacar) : array{
-    if ($valorASacar > $conta['saldo']){
-        exibeMensagem("Você não pode sacar este valor");
-    } else{
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
-
-function depositar(array $conta, float $valorADepositar): array{
-    if ($valorADepositar > 0){
-    $conta['saldo'] += $valorADepositar;
-    } else{
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-    return $conta;
-}
-
-function exibeMensagem(string $mensagem): void{
-    echo $mensagem . PHP_EOL;
-}
-
 $contasCorrentes['123.456.789-10'] = depositar($contasCorrentes['123.456.789-10'], 900);
 $contasCorrentes['142.356.789-11'] = sacar($contasCorrentes['142.356.789-11'], 500);
 $contasCorrentes['765.432.189-41'] = sacar($contasCorrentes['765.432.189-41'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . " " . $conta['saldo']);
+    exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
 }
 
 ?>
